cf9124cb500b9e0635f2caad02dffa7d503cd354
nadirabbas72 <149385075+nadirabbas72@users.noreply.github.com>
2023-11-01 08:00:53 +0000

    font size update 15

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 14453
+let accountEmail = "nadirabbas72@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+// accountId=2 not allowed
+console.log(accountId)
+accountEmail="ht@gmail.com"
+accountPassword="5486"
+accountCity="fsd"
+console.table([accountId,accountEmail,accountPassword,accountCity])
